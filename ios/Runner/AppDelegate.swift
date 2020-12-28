0eb043a895e1a824b835a5f4a7ce5472a28e5a02
mohamed <mohamed.hassan.1993y@gmail.com>
2020-12-28 03:36:09 -0800

    before change xcode notification setting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,9 +10,16 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     GMSServices.provideAPIKey("AIzaSyCTRCoMIE-547qQy9KKP4c77nnNzbZcJqQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+       Messaging.messaging().apnsToken = deviceToken
+       super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+     }
 }
--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -12,17 +12,13 @@ class PushNotificationServices {
       }
 
     _fcm.configure(
-      onBackgroundMessage:  (Map<String, dynamic> message) async {
-        print("onBackgroundMessage: $message");
-      },
       onLaunch: (Map<String, dynamic> message) async {
         print("onLaunch: $message");
       },
-
       onMessage: (Map<String, dynamic> message) async {
         print("onMessage: $message");
-      },
 
+      },
       onResume: (Map<String, dynamic> message) async {
         print("onResume: $message");
       },
