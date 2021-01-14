da7625e0439fa2110093fc2e7eedae1e4b8b0079
mohamed <mohamed.hassan.1993y@gmail.com>
2021-01-14 06:17:48 -0800

    add attentance and pick up sections on account_tab.dart collect all url server on servers_links.dart

--- lib/screens/tabs/account_tab.dart
+++ lib/screens/tabs/account_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:profileapp/constants.dart';
 import 'package:profileapp/screens/details/web_view_page.dart';
+import 'package:profileapp/servers/servers_links.dart';
 
 class AccountTab extends StatefulWidget {
   @override
@@ -11,11 +12,52 @@ class AccountTab extends StatefulWidget {
 class _AccountTabState extends State<AccountTab> {
   @override
   Widget build(BuildContext context) {
+    Row accountBtn({@required name ,@required kDirectink })
+    {
+      return  Row(
+
+        children: [
+          SizedBox(width: 10,),
+          Expanded(
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+
+              ),
+              color: kStartCyanColor,
+
+              child:Text(name ,style: TextStyle(  color: Colors.white,),),
+
+              onPressed: () {  Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailsWebView(webURL: kDirectink,)),
+              );
+              },
+            ),
+          ),
+          SizedBox(width: 10,),
+        ],
+      );
+    }
     return Scaffold(
-      body: DetailsWebView(
-        webURL: "https://204.15.178.113:8444/Login",
-        isAppBar: false,
+      body: Container(
+        child: Column(
+          children: [
+            accountBtn(kDirectink:kAttendance,name: "Attendance"),
+            accountBtn(kDirectink:kPickUp,name: "Pick-Up"),
+
+            
+          ],
+        ),
       ),
     );
+
+
+
+
   }
+
+
+
 }
+
---
+++ lib/servers/servers_links.dart
@@ -0,0 +1,6 @@
+
+
+const String serverHost = "https://sharepoint.pclink.com.eg";
+
+const String kAttendance = serverHost+":8444";
+const String kPickUp = serverHost+":8443";
